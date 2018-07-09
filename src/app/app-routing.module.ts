d54ce7825be2dd0e06cbf6974f8501c33ff73889
rk-10 <khanejarohan07@gmail.com>
2018-07-09 10:43:47 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { HeroesComponent } from "./heroes/heroes.component";
 
-@NgModule({
-  exports: [ RouterModule ]
-})
-export class AppRoutingModule { }
-
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent}
 ];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes)]
+})
+
+export class AppRoutingModule { }
